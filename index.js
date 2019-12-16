8cd98d2c4dedeb0ebedeb1f23ea90f5774b0455a
Aaberte <aaron_bertelsen@yahoo.com>
2019-12-16 14:00:46 -0800

    Smart Person tests passed

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ const person = {
 };
 
 
-
 /**
  * ### Challenge `makeSmartPerson`
  * 
@@ -83,9 +82,20 @@ const person = {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const Person = {
+    name: name,
+    sum: function(num1, num2) {
+      return num1 + num2;
+    },
+    speak: function() {
+      return `Hello, my name is ${name}`;
+    } 
+  };
+  return Person;
 }
+const smartHuman = makeSmartPerson("Turing");
+console.log(smartHuman);
 
 
 
